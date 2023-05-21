48bc117d774906eb89a01412c7dd9a9ef1862190
natalia <lia921lia@gmail.com>
2023-05-20 21:53:37 -0400

    HomeWork

---
+++ src/main/java/lesson2/HomeWork/HomeWorkTask2.java
@@ -0,0 +1,16 @@
+package lesson2.HomeWork;
+
+public class HomeWorkTask2 {
+
+    public  static  void  main(String[] args) {
+        int number = 354;
+        int e = number %10;
+        int d = number /10 % 10;
+        int s  = number / 100;
+
+        System.out.printf("s");
+        System.out.printf("d");
+        System.out.printf("e");
+
+    }
+}
---
+++ src/main/java/lesson2/HomeWork/HomeWorkТаск1.java
@@ -0,0 +1,21 @@
+package lesson2.HomeWork;
+
+public class HomeWorkТаск1 {
+    public static void main(String[] args) {
+        String ch = "G";
+        int in = 89;
+        byte bt = 4;
+        short sh =56;
+        float fl = 4.73334436F;
+        double db = 4.3554533532;
+        long lg = 12121L;
+
+        System.out.printf("char:" + ch);
+        System.out.printf("integer:" + in);
+        System.out.printf("byte:" + bt);
+        System.out.printf("short:" + sh);
+        System.out.printf("float:" + fl);
+        System.out.printf("double:" +db);
+        System.out.printf("long:" + lg);
+    }
+}
